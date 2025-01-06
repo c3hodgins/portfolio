4618e1a74c6ff35c04b25a564371cd1616da48c4
c3hodgins <c3hodgins@gmail.com>
2025-01-06 18:06:11 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [section, setSection] = useState('About')
+
+  const handleSection = (sect) => {
+    setSection(sect);
+  }
+
+  
+  return (
+    <>
+      <div className='flex justify-between  top-0'>
+        <h1>Charles Hodgins</h1>
+        <div className = 'gap-2 flex justify-between'></div>
+        <h2 onClick={() => handleSection('About')}>About</h2>
+        <h2 onClick={() => handleSection('Projects')}>Projects</h2>
+        <h2 onClick={() => handleSection('Contact')}>Contact</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
