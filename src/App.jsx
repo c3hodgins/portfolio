2330a61670d243a5a39b0ebe06643d004ff6c0f2
c3hodgins <c3hodgins@gmail.com>
2025-01-07 02:29:48 -0500

    *

--- src/App.jsx
+++ src/App.jsx
@@ -18,20 +18,25 @@ function App() {
         section={section}
         sections={sections}
       ></Header>
-      <div className="bg-slate-700 flex flex-col justify-center items-center">
+      <div className="pt-24 bg-slate-700 flex flex-col justify-center items-center">
         {/*About*/}
-        {section == 'About' && <div className="text-xl m-10 w-3/4 p-5 bg-white">
-          <h1>About</h1>
-        </div>}
+        {section == "About" && (
+          <div className="text-xl m-10 w-11/12 p-5 h-screen bg-white">
+            <h1>About</h1>
+          </div>
+        )}
         {/* Projects */}
-        {section == 'Projects' &&
-        <div className="text-xl m-10 w-3/4 p-5 bg-white">
-          <h1>Projects</h1>
-        </div>}
+        {section == "Projects" && (
+          <div className="text-xl shadow-inner m-10 w-11/12 p-5 bg-white">
+            <h1>Projects</h1>
+          </div>
+        )}
         {/* Contact */}
-        {section == 'Contact' && <div className={"shadow-inner text-xl m-10 w-3/4 p-5 bg-white"}>
-          <h1 className={section == "Contact"  ? 'opacity-100' : 'opacity-0' +' duration-200'} >Contact</h1>
-        </div>}
+        {section == "Contact" && (
+          <div className={"shadow-lg text-xl m-10 w-11/12 p-5 bg-white"}>
+            <h1>Contact</h1>
+          </div>
+        )}
       </div>
     </>
   );
--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ export const Header = ({ section, sections, handleSection }) => {
   }, []);
 
   return (
-    <div className="sticky items-center m-0 px-10 w-full h-24 flex justify-between border-b-4 text-white border-slate-700 bg-slate-600">
+    <div className="header">
       <h1 className="lg:text-2xl sm:text-md">Charles Hodgins</h1>
       <div className="w-1/2 flex h-full items-center justify-evenly">
         {sections.map((sect) => (
@@ -15,7 +15,7 @@ export const Header = ({ section, sections, handleSection }) => {
             key={sect}
             className={
               (section === sect ? " bg-slate-700 " : "bg-slate-600 ") +
-              `sm:text-sm p-4 lg:text-lg duration-300 hover:bg-slate-800  ease-in-out`
+              `sm:text-xs text-xs p-2 lg:p-4 lg:text-lg duration-300 hover:bg-slate-800  ease-in-out`
             }
             onClick={() => handleSection(sect)}
           >
